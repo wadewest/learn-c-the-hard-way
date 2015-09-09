25783ebabb707328a1082c75ef233ead7874bbf2
Wade West <wwest81@gmail.com>
2015-09-09 20:08:33 +0000

    Exercise 11: While-Loop and Boolean Expressions

--- main.c
+++ main.c
@@ -2,13 +2,13 @@
 
 int main(int argc, char *argv[])
 {
-  int i = 0;
-  
   // go through each string in argv
-  // why am I skipping argv[0]?
-  for(i = 1; i < argc; i++)
+  
+  int i = 1; // argv[0] is the name of the program
+  while( i < argc )
   {
     printf("arg %d: %s\n", i, argv[i]);
+    i++;
   }
   
   // Let's make our own array of strings
@@ -16,11 +16,13 @@ int main(int argc, char *argv[])
     "California", "Oregon",
     "Washington", "Texas"
   };
-  int num_states = 4;
   
-  for(i = 0; i < num_states; i++)
+  int num_states = 4;
+  i = 0; // watch for this
+  while( i < num_states )
   {
     printf("state %d: %s\n", i, states[i]);
+    i++;
   }
   
   return 0;
