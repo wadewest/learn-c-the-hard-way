79c54d48ac23abd93a5ea8432b46a0cbccc96be6
Wade West <wwest81@gmail.com>
2015-09-16 22:38:08 -0500

    Extra Credit

--- main.c
+++ main.c
@@ -2,7 +2,11 @@
 
 int main(int argc, char *argv[])
 {
-    puts("Hello World.");
-    
-    return 0;
+  fputs("This message is written to stderr.\n", stderr);
+  puts("Hello World.");
+  puts("puts is cool and all");
+  puts("but not a lot it can do");
+  fputs("other than write to streams\n", stdout);
+  puts("for that it's great");
+  return 0;
 }
